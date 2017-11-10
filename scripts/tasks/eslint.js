85a44ef5b37f0dca89c48a4f4c75c49d036d256b
Joe Lim <xjlim@users.noreply.github.com>
2017-11-10 11:57:38 -0800

    refactor lint scripts into one

--- scripts/tasks/eslint.js
+++ scripts/tasks/eslint.js
@@ -7,24 +7,55 @@
 
 'use strict';
 
-var path = require('path');
-var spawn = require('child_process').spawn;
+const execFileSync = require('child_process').execFileSync;
+const CLIEngine = require('eslint').CLIEngine;
 
-var extension = process.platform === 'win32' ? '.cmd' : '';
+const cli = new CLIEngine();
+const formatter = cli.getFormatter();
 
-spawn(
-  path.join('node_modules', '.bin', 'eslint' + extension),
-  ['.', '--max-warnings=0'],
-  {
-    // Allow colors to pass through
-    stdio: 'inherit',
-  }
-).on('close', function(code) {
-  if (code !== 0) {
-    console.error('Lint failed');
+const isLintChanged = process.argv[2] === 'changed';
+const report = cli.executeOnFiles(isLintChanged ? getChangedFiles() : ['.']);
+console.log(formatter(report.results));
+
+if (isLintChanged) {
+  handleResultForLintChanged();
+} else {
+  handleResultForLint();
+}
+
+function handleResultForLint() {
+  if (report.warningCount > 0 || report.errorCount > 0) {
+    console.log('Lint failed.');
+    process.exit(1);
   } else {
-    console.log('Lint passed');
+    console.log('Lint passed.');
   }
+}
 
-  process.exit(code);
-});
+function handleResultForLintChanged() {
+  if (report.errorCount > 0) {
+    console.log('Lint failed for changed files.');
+    process.exit(1);
+  } else {
+    console.log('Lint passed for changed files.');
+  }
+}
+
+function getChangedFiles() {
+  const mergeBase = execFileSync('git', ['merge-base', 'HEAD', 'master'], {
+    stdio: 'pipe',
+    encoding: 'utf-8',
+  }).trim();
+  const changedFiles = execFileSync(
+    'git',
+    ['diff', '--name-only', '--diff-filter=ACMRTUB', mergeBase],
+    {
+      stdio: 'pipe',
+      encoding: 'utf-8',
+    }
+  )
+    .trim()
+    .toString()
+    .split('\n');
+  return changedFiles.filter(file => file.match(/.js$/g));
+}
